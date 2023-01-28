99af4cc114b900457b8930d30911b91f7474513d
Ana Aikman <aikmanfamily@gmail.com>
2023-01-28 01:59:47 -0600

    Added list of each rhyme for basic A I U E O syllables. Added function that converts hiragana input to katakana, then attempted to add another function that tests for each list of rhymes (it did not work the way i wanted it to.) Also added comments to functions to make it easier to understand what they do.

--- rhymeyomi.py
+++ rhymeyomi.py
@@ -1,21 +1,52 @@
 ﻿import tkinter as tk
 import tkextrafont as tkfont
+import jaconv as jv
+
+arhyme = ["ア", "カ", "ガ","サ", "ザ", "ハ", "バ", "パ", "ラ", "ワ", "マ", "ナ", "タ", "ダ", "ヤ", "チャ", "ファ", "ヴァ"]
+irhyme = ["イ", "キ", "ギ","シ", "ジ", "チ", "ヂ", "ニ", "ヒ", "ビ", "ピ", "ミ", "リ", "フィ", "ディ", "ウィ", "ティ", "ヴぃ"]
+urhyme = ["ウ", "ク", "グ","ス", "ズ", "ツ", "ヅ", "ヌ", "フ", "ブ", "プ", "ム", "ル", "ユ", "キュ", "チュ", "ヴ", "シュ", "リュ"]
+erhyme = ["エ", "ケ", "ゲ","セ", "ゼ", "テ", "デ", "ネ", "ヘ", "ベ", "ペ", "メ", "レ", "イェ", "フェ", "シェ", "チェ", "シュ", "ウェ", "ヴェ"]
+orhyme = ["オ", "コ", "ゴ","ソ", "ゾ", "ト", "ド", "ノ", "ホ", "ボ", "ポ", "モ", "ロ", "ヨ", "ヲ", "ショ", "チョ", "フォ", "ヴォ"]
+
 
 window = tk.Tk()
-window.title("RhymeYomi ALPHA")
+window.title("ライム読み ALPHA")
 
-def kensaku():
-    search = kanahara.get()
-    searchtoiu = tk.Label(font=font,text=f"検索は：{search}")
-    searchtoiu.grid(row=3, column=1, sticky="ew", padx=5, pady=5)
+
+def kensakustart():     #Displays search onscreen and adds results field
+    origsearch = kanahara.get()
+    searchtoiu = tk.Label(font=font,text=f"検索は：{origsearch}")
+    searchtoiu.grid(row=3, column=2, sticky="ew", padx=1, pady=1)
     kekkahara = tk.Text()
-    kekkahara.grid(row=5, column=1, sticky="ew", padx=5, pady=5)
+    kekkahara.grid(row=4, column=2, sticky="ew", padx=1, pady=1)
+    search = jv.hira2kata(origsearch)
+    kensaku(search)
 
-window.geometry("600x600")
+def kensaku(search):        #Searches for rhymes in dictionary
+    searchlist = search[::1]
+    for x in searchlist:
+        if x in arhyme:
+            x = 0
+        else:
+            if x in irhyme:
+                x = 1
+            else:
+                if x in urhyme:
+                    x = 2
+                else:
+                    if x in erhyme:
+                        x = 3
+                    else:
+                        if x in orhyme:
+                            x = 4
+
+
+
+window.geometry("800x600")
 font = tkfont.Font(file="font\digi.ttf", family="UD Digi Kyokasho N-R")
 writekana = tk.Label(font=font,text="カナ文字で言葉を書いてください！")
 kanahara = tk.Entry(window)
-botan = tk.Button(text="検索", command=kensaku)
+botan = tk.Button(text="検索", command=kensakustart)
 
 writekana.grid(row=1, column=1, sticky="e", padx=5, pady=5)
 kanahara.grid(row=2, column=1, sticky="e", padx=5, pady=5)
